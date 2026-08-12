167d4de2a9b84fa0155476b8b8015a41d65c0cab
syedhanzala <syedhanzal17@gmail.com>
2026-08-12 14:31:35 +0530

    auto

--- gcd.js
+++ gcd.js
@@ -23,15 +23,15 @@
 
 
 
-// let num1=50;
-// let num2=20;
-// let gcd = 1;
-// while (num2 !== 0) {
-//     gcd = num2;
-//     num2 = num1 % num2;
-//     num1 = gcd;
-// }
-// console.log("GCD is: " + gcd);
+let num1=50;
+let num2=20;
+let gcd = 1;
+while (num2 !== 0) {
+    gcd = num2;
+    num2 = num1 % num2;
+    num1 = gcd;
+}
+console.log("GCD is: " + gcd);
 
 
 
