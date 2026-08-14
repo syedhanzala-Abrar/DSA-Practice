a6b5185d2f7ea885add3c5e082f3bf017171aca6
syedhanzala <syedhanzal17@gmail.com>
2026-08-14 15:42:27 +0530

    ,,,,

--- arrangingelement.js
+++ arrangingelement.js
@@ -25,3 +25,35 @@ const mergeArrays = (x, y) => {
     return result;
 };
 console.log(mergeArrays(x, y));
+
+
+// let x=[5,5,5,6,6,7];
+// let y=[1,2,2,6,7,9,11,13,13];
+// let i=0;
+// let j=0;
+// let result=[];
+// const mergeArrays = (x, y) => {
+//     while (i < x.length && j < y.length) {
+//         if (x[i] < y[j]) {
+//             result.push(x[i]);
+//             i++;
+//         } else {
+//             result.push(y[j]);
+//             j++;
+//         }
+//     }
+//     // Push any remaining elements from either array
+//     while (i < x.length) {
+//         result.push(x[i]);
+//         i++;
+//     }
+//     while (j < y.length) {
+//         result.push(y[j]);
+//         j++;
+//     }
+//     return result;
+// };
+// console.log("Array x:", x);
+// console.log("Array y:", y);
+// console.log("Merged Array:", mergeArrays(x, y));
+
