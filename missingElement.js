23f47ad016ffc1841a66808bb2a7ee2efa0f743f
syedhanzala <syedhanzal17@gmail.com>
2026-08-14 15:27:23 +0530

    xor

---
+++ missingElement.js
@@ -0,0 +1,13 @@
+// let arr=[6, 4, 5, 2, 1];
+// let n = arr.length;
+// let totalSum = (n * (n + 1)) / 2;
+// let arrSum = 0;
+// for (let i = 0; i < n; i++) {
+//     arrSum += arr[i];
+// }
+// let missingElement = totalSum - arrSum;
+// console.log(missingElement);
+
+
+
+
